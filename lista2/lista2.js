f3798f583492d5d4be89c59b980465205d3ca8c7
Victor Vieira <victorxdking@gmail.com>
2023-08-29 11:05:27 -0300

    Aula 29/08

---
+++ lista2/lista2.js
@@ -0,0 +1,196 @@
+function exe5() {
+    // recupera os dados do html
+    let escolha = Number(document.getElementById("opcao").value);
+    let nro1 = Number(document.getElementById("nro1").value);
+    let nro2 = Number(document.getElementById("nro2").value); // Corrigido de nro1
+    let resultado = null;
+
+    switch (escolha) {
+        case 1:
+            resultado = (nro1 + nro2) / 2;
+            break;
+        case 2:
+            resultado = Math.abs(nro1 - nro2);
+            break;
+        case 3:
+            resultado = nro1 * nro2;
+            break;
+        case 4:
+            if (nro2 !== 0) {
+                resultado = nro1 / nro2;
+            } else {
+                document.getElementById("resultado").innerHTML = 
+                "Divisão por zero não pode"
+            }
+            break;
+        default: document.getElementById("resultado").innerHTML = 
+        'Operação inválida'
+    }
+
+    if (resultado != null) {
+        document.getElementById("resultado").innerHTML = 
+        `RESULTADO = ${resultado}`
+    }
+}
+function exe22() {
+    let idade = Number(document.getElementById("idade").value)
+    let peso = Number(document.getElementById("peso").value)
+    let risco = null
+    // devolve o resultado
+    if (idade >= 0 && idade < 20) {
+        if (peso >= 0 && peso <= 60) {
+            risco = 9
+        } 
+        else if (peso >= 60 && peso <= 90){
+            risco = 8 
+        }
+        else if (peso > 90){
+            risco = 7 
+        }
+        else {
+            document.getElementById("risco").innerHTML = "Peso negativo"
+        }
+    }
+    else if (idade >= 20 && idade <= 50) {
+        if (peso >= 0 && peso <= 60) {
+            risco = 6
+        } 
+        else if (peso >= 60 && peso <= 90){
+            risco = 5 
+        }
+        else if (peso > 90){
+            risco = 4 
+        }
+        else {
+            document.getElementById("risco").innerHTML = "Peso negativo"
+        }       
+    }
+    else if (idade > 50){
+        if (peso >= 0 && peso <= 60) {
+            risco = 6
+        } 
+        else if (peso >= 60 && peso <= 90){
+            risco = 5 
+        }
+        else if (peso > 90){
+            risco = 4 
+        }
+        else {
+            document.getElementById("risco").innerHTML = "Peso negativo"
+        }   
+    }
+    else {
+        document.getElementById("risco").innerHTML = "Idade negativa"
+    }
+    if (risco != null) {
+        document.getElementById("risco").innerHTML = 
+        `O Risco é ${risco}`
+    }
+}
+function exe23() {
+        // recupera os dados do html
+        let codigo = Number(document.getElementById("codigo").value)
+        let qtde = Number(document.getElementById("qtde").value)
+        // calcula preço unitário
+        let precoUnitario = null
+        if (codigo >= 1 && codigo <= 10){
+            precoUnitario = 10
+        }
+        else if (codigo >= 11 && codigo <= 20){
+            precoUnitario = 15
+        }
+        else if (codigo >= 21 && codigo <= 30){
+            precoUnitario = 20
+        }
+        else if (codigo >= 31 && codigo <= 40){
+            precoUnitario = 30
+        }
+        else {
+            document.getElementById("resultado").innerHTML = "Código inválido"
+        }
+        // preco total
+        let precoTotal
+        if (precoTotal != null && qtde >= 0){
+            precoTotal = qtde * precoUnitario
+            // calcula o desconto
+            let desconto
+            if (precoTotal < 250){
+                desconto = precoTotal * 5 / 100    
+            }
+            else if (precoTotal >= 250 && precoTotal <= 500){
+                desconto = precoTotal * 10 / 100
+            }
+            else {
+                desconto = precoTotal * 15 / 100
+            }
+            // calcula o preço final
+            let precoFinal = precoTotal - desconto
+            // exibe o resultado
+            document.getElementById("resultado").innerHTML = `Preço Unitário  ${precoUnitario} 
+            Preço Total ${precoTotal}`
+            // exibe o resultado do desconto
+            document.getElementById("desconto").innerHTML = `Desconto ${desconto}`
+        }
+        else {
+            document.getElementById("resultado").innerHTML = "Código e/ou qtde inválido(s)"
+        }
+}
+function exe24() {
+    let preco = Number(document.getElementById("preco").value)
+    let categoria = Number(document.getElementById("catergoria").value)
+    let situacao = Number(document.getElementById("situacao").value)
+    if (preco < 0){
+        document.getElementById("resultado").innerHTML = "Preço inválido"
+    }
+    else {
+        // calcula aumento
+        let aumento
+        switch(categoria){
+            case 1: if (preco <= 25){
+                aumento = preco * 5 / 100
+            }
+            else {
+                aumento = preco * 12 / 100
+            }
+            break
+            case 2: if (preco <= 25){
+                aumento = preco * 8 / 100
+            }
+            else {
+                aumento = preco * 15 / 100
+            }
+            break
+            case 3: if (preco <= 25){
+                aumento = preco * 10 / 100
+            }
+            else {
+                aumento = preco * 18 / 100
+            }
+            break
+        }
+        // calcula imposto
+        let imposto
+        if (categoria == 2 || situacao == 'R'){
+            imposto = preco * 5 / 100
+        }
+        else {
+            imposto = preco * 8 / 100
+        }
+        // calcula novo preço
+        let novoPreco = preco + aumento - imposto
+        // calcula classificação
+        let classificacao
+        if (novoPreco < 50){
+            classificacao = "Barato"
+        }
+        else if (novoPreco >= 50 && novoPreco < 120){
+            classificacao = "Normal"
+        }
+        else {
+            classificao = "Caro"
+        }
+        // resultado
+        document.getElementById("resultado").innerHTML = `Aumento ${aumento} Desconto 
+        ${desconto} Novo Preço ${novoPreco} e Classificação ${classificacao}`
+    }
+}
