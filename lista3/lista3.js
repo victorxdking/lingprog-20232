1d35a23c7d3d9906d8a0ae0d0815315f83be51c8
victorxdking <victorxdking@gmail.com>
2023-09-04 20:27:10 -0300

    exercício 1 finalizado

--- lista3/lista3.js
+++ lista3/lista3.js
@@ -1,30 +1,30 @@
 function exe1() {
     let contador2 = 1
-    while (contador2 <= 5){
+    while (contador2 <= 5) {
         let a = Number(prompt('Informe A'))
         let b = Number(prompt('Informe B'))
         let c = Number(prompt('Informe C'))
         let d = Number(prompt('Informe D'))
-        let contador = 1
-        
-        while (contador <=3) {
-            
-            if (a > b) {
-                let aux = a
-                a = b
-                b = aux
-            }
-            if (b > c) {
-                let aux = b
-                b = c
-                c = aux
-            }
-            if (c > d) {
-                let aux = c
-                c = d
-                d = aux
-            }
+    // organiza os valores em ordem crescente
+    while (a > b || b > c || c > d) {
+        if (a > b) {
+            let aux = a
+            a = b
+            b = aux
         }
-        contador++
+        if (b > c) {
+            let aux = b
+            b = c
+            c = aux
+        }
+        if (c > d) {
+            c = d
+            d = aux
+        }
+    }
+    // mostra os valores organizados
+    console.log(`Ordem Crescente - ${a}, ${b}, ${c}, ${d}`)
+    console.log(`Ordem Decrescente - ${d}, ${c}, ${b}, ${a}`)
+    contador2++
     }
 }
