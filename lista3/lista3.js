344574ac267bd44bcb4fff85b24f172d7773c446
Victor Vieira <victorxdking@gmail.com>
2023-09-05 10:54:27 -0300

    aula 05/09

--- lista3/lista3.js
+++ lista3/lista3.js
@@ -28,7 +28,7 @@ function exe1() {
     contador2++
     }
 }
-function exe2() {
+/*function exe2() {
     let precoIngresso = 5.0
     let quantidadeIngressos = 120
     const despesa = 200.0
@@ -41,7 +41,7 @@ function exe2() {
     while (precoIngresso >= 1.0) {
         const lucro = (precoIngresso * quantidadeIngressos) - despesa
 
-        console.log(`${precoIngresso.toFixed(2)} | ${quantidadeIngressos} | R$${lucro.toFixed(2)}`)
+        console.log(`${precoIngresso.toFixed(2)}               | ${quantidadeIngressos}            |           R$${lucro.toFixed(2)}`)
 
         if (lucro > lucroMaximo) {
             lucroMaximo = lucro
@@ -56,4 +56,30 @@ function exe2() {
     console.log(`\nLucro máximo esperado: R$${lucroMaximo.toFixed(2)}`)
     console.log(`Preço do ingresso para lucro máximo: R$${precoLucroMaximo.toFixed(2)}`)
     console.log(`Quantidade de ingressos vendidos para lucro máximo: ${quantidadeLucroMaximo}`)
+}*/
+function exe2() {
+    let qtde = 120;
+    const custo = 200;
+    let corpo = '';
+    for (let preco = 5; preco >= 1; preco -= 0.5) {
+      let lucro = (preco * qtde) - custo;
+      corpo += `<tr> <td> ${preco.toFixed(2)} </td> <td> ${qtde} </td> <td>
+        ${custo.toFixed(2)} <td> ${lucro.toFixed(2)} </td> </tr>`;
+      qtde += 26;
+    }
+    document.getElementById('corpo').innerHTML = corpo;
+  }
+function exe5() {
+    let resposta = ``
+    for (let i = 1; i <= 10; i++){
+        for(let j = 1; j <= 10; j++){
+            let multiplicacao = i * j
+            resposta = resposta + `${i} x ${j} = ${multiplicacao} <br>` 
+        }
+        resposta = resposta + `<br>`
+    }
+    document.getElementById('resultado').innerHTML = resposta
 }
+
+
+  
