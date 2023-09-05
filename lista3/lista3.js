aa38e102e91a35fde1b87d9015473b3f328f5f4c
victorxdking <victorxdking@gmail.com>
2023-09-05 18:15:43 -0300

    aula 05/09

--- lista3/lista3.js
+++ lista3/lista3.js
@@ -5,81 +5,71 @@ function exe1() {
         let b = Number(prompt('Informe B'))
         let c = Number(prompt('Informe C'))
         let d = Number(prompt('Informe D'))
-    // organiza os valores em ordem crescente
-    while (a > b || b > c || c > d) {
-        if (a > b) {
-            let aux = a
-            a = b
-            b = aux
-        }
-        if (b > c) {
-            let aux = b
-            b = c
-            c = aux
-        }
-        if (c > d) {
-            c = d
-            d = aux
-        }
-    }
-    // mostra os valores organizados
-    console.log(`Ordem Crescente - ${a}, ${b}, ${c}, ${d}`)
-    console.log(`Ordem Decrescente - ${d}, ${c}, ${b}, ${a}`)
-    contador2++
-    }
+        let contador = 1
+        while (contador <= 3) {
+            if (a > b) { // troca A com B
+                let aux = a
+                a = b
+                b = aux
+            }
+            if (b > c) { // troca B com C
+                let aux = b
+                b = c
+                c = aux
+            }
+            if (c > d) { // troca C com D
+                let aux = c
+                c = d
+                d = aux
+            }
+            contador++
+        } // fecha while
+        console.log(`Ordem Crescente ${a}, ${b}, ${c}, ${d}`)
+        console.log(`Ordem Decrescente ${d}, ${c}, ${b}, ${a}`)
+        contador2++
+    } // fecha while
 }
-/*function exe2() {
-    let precoIngresso = 5.0
-    let quantidadeIngressos = 120
-    const despesa = 200.0
-    let lucroMaximo = 0.0
-    let precoLucroMaximo = 0.0
-    let quantidadeLucroMaximo = 0
-
-    console.log("Preço do Ingresso | Quantidade de Ingressos | Lucro")
-
-    while (precoIngresso >= 1.0) {
-        const lucro = (precoIngresso * quantidadeIngressos) - despesa
-
-        console.log(`${precoIngresso.toFixed(2)}               | ${quantidadeIngressos}            |           R$${lucro.toFixed(2)}`)
-
-        if (lucro > lucroMaximo) {
-            lucroMaximo = lucro
-            precoLucroMaximo = precoIngresso
-            quantidadeLucroMaximo = quantidadeIngressos
-        }
-
-        precoIngresso -= 0.5
-        quantidadeIngressos += 26
-    }
-
-    console.log(`\nLucro máximo esperado: R$${lucroMaximo.toFixed(2)}`)
-    console.log(`Preço do ingresso para lucro máximo: R$${precoLucroMaximo.toFixed(2)}`)
-    console.log(`Quantidade de ingressos vendidos para lucro máximo: ${quantidadeLucroMaximo}`)
-}*/
 function exe2() {
-    let qtde = 120;
-    const custo = 200;
-    let corpo = '';
-    for (let preco = 5; preco >= 1; preco -= 0.5) {
-      let lucro = (preco * qtde) - custo;
-      corpo += `<tr> <td> ${preco.toFixed(2)} </td> <td> ${qtde} </td> <td>
-        ${custo.toFixed(2)} <td> ${lucro.toFixed(2)} </td> </tr>`;
-      qtde += 26;
-    }
-    document.getElementById('corpo').innerHTML = corpo;
-  }
-function exe5() {
-    let resposta = ``
-    for (let i = 1; i <= 10; i++){
-        for(let j = 1; j <= 10; j++){
-            let multiplicacao = i * j
-            resposta = resposta + `${i} x ${j} = ${multiplicacao} <br>` 
+    let qtde = 120
+    const custo = 200
+    let corpo = ''
+    let lucro = 0
+    let maiorLucro = 0
+    let melhorQtde = 0
+    let melhorPreco = 0
+    for(let preco = 5; preco >= 1; preco = preco - 0.5){
+        lucro = (preco * qtde) - custo
+        if(lucro > maiorLucro){
+            maiorLucro = lucro
+            melhorPreco = preco
+            melhorQtde = qtde
         }
-        resposta = resposta + `<br>`
+        corpo = corpo + `<tr> <td> ${preco.toFixed(2)} </td> <td> ${qtde} </td> <td> ${custo.toFixed(2)} </td> <td> ${lucro.toFixed(2)} </td> </tr>`
+        qtde = qtde + 26
     }
-    document.getElementById('resultado').innerHTML = resposta
+    document.getElementById('corpo').innerHTML = corpo
+    document.getElementById('maiorLucro').innerHTML = `Maior lucro = R$ ${maiorLucro.toFixed(2)}`
+    document.getElementById('melhorQtde').innerHTML = `Melhor Quantidade = ${melhorQtde}`
+    document.getElementById('melhorPreco').innerHTML = `Melhor Preço = R$ ${melhorPreco.toFixed(2)}`
 }
-
-
+function exe5(){
+    let corpo = ''
+    for(let numero = 1; numero <= 10; numero++){
+        corpo = corpo + `
+        <tr> 
+        <td> Tabuada </td> 
+        <td> do </td> 
+        <td> ${numero}</td>
+        </tr>`
+        for(let aux = 1; aux <= 10; aux++){
+            corpo = corpo + `
+            <tr> 
+            <td> ${numero} </td> 
+            <td> ${aux} </td> 
+            <td> ${numero * aux}</td>
+            </tr>`
+        }
+    }
+    document.getElementById('corpo').innerHTML = corpo
+}
   
