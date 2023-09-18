3c10a0e5f3257cd57d5159a3d84cb36371f79731
Victor Vieira <victorxdking@gmail.com>
2023-09-18 09:04:47 -0300

    aula 18/09 professor Daniel

--- lista3/lista3.js
+++ lista3/lista3.js
@@ -432,10 +432,77 @@ function calcularMaiorMenorNota() {
     alert(`A maior nota foi de ${maior} com o nome de ${maiorNome}`)
     alert(`A menor nota foi de ${menor} com o nome de ${menorNome}`)
 }
-
-
-
-
-
-
- 
+function exe3() {
+    let codigos = []
+    let estoque = []
+    // entrada de dados
+    for(let i=0;i<2;i++){
+        codigos.push(Number(prompt(`Informe o código do produto ${i+1}`)))
+        estoque.push(Number(prompt(`Informe a quantidade em estoque do produto ${i+1}`)))
+    }
+    // compra por um cliente
+    let cliente = Number(prompt(`Informe o código do cliente que deseja comprar`))
+    do {
+        let produto = Number(prompt(`Informe o código do produto que deseja comprar`))
+        let qtde = Number(prompt(`Informe a quantidade que deseja comprar`))
+        // verificando se o produto existe e se tem estoque
+        let achou = false // produto não foi encontrado
+        for(let i = 0; i<2; i++){
+            if (codigos[i] == produto){ // econtrou o produto
+                achou = true // achou o produto
+                if(estoque[i] >= qtde){ // tem estoque
+                    alert(`Venda realizada com sucessso`)
+                    estoque[i] = estoque[i] - qtde // atualiza estoque
+                }
+                else {
+                    alert(`Não tem estoque suficiente`)
+                }
+            }
+        }
+        if(!achou){ // não econtrou o produto
+            alert(`Produto não encontrado`)
+        }
+        cliente = Number(prompt(`Informe o código do novo cliente ou 0 para encerrar`))   
+    }
+    while(cliente !=0)
+    // mostra o resultado
+    for(let i=0;i<10;i++){
+        console.log(`Produto ${codigos[i]} tem ${estoque[i]} unidades em estoque`)
+    }
+}
+function exe6() {
+    let vendas = []
+    let percentuais = []
+    let nomes = []
+    // entrada de dados
+    for(let i=0;i<10;i++){
+        nomes.push(prompt(`Informe o nome do vendedor ${i+1}`))
+        vendas.push(Number(prompt(`Informe o total de vendas do vendedor ${i+1}`)))
+        percentuais.push(Number(prompt(`Informe o percentual de comissão do vendedor ${i+1}`)))
+    }
+    // quanto vai receber de comissão
+    let comissoes = []
+    for(let i=0;i<10;i++){
+        comissoes.push(vendas[i] * percentuais[i]/100)
+        console.log(`${nomes[i]} vai receber ${comissoes[i]}`)
+    }
+    // total de vendas de todos os vendedores
+    let total = 0
+    for(let i=0;i<10;i++){
+        total = total + vendas[i]
+    }
+    console.log(`Total de vendas ${total}`)
+    // maior e menor valor a receber, e os nomes dos vendedores
+    let maior = comissoes[0]
+    let menor = comissoes[0]
+    for(let i=1;i<10;i++){
+        if(comissoes[i] > maior){
+            maior = comissoes[i]
+        }
+        if(comissoes[i] < menor){
+            menor = comissoes[i]
+        }
+    }
+    console.log(`Maior comissão ${maior} do ${nomes[comissoes.indexOf(maior)]}`)
+    console.log(`Maior comissão ${menor} do ${nomes[comissoes.indexOf(menor)]}`)
+}
