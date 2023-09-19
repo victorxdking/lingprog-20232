779857d8d6f881303aaaa9cc0c914ca8009b5105
Victor Vieira <victorxdking@gmail.com>
2023-09-19 11:11:10 -0300

    prova 19/09

--- lista4/lista4.js
+++ lista4/lista4.js
@@ -228,3 +228,106 @@ function exe6() {
     console.log(`Maior comissão ${maior} do ${nomes[comissoes.indexOf(maior)]}`)
     console.log(`Maior comissão ${menor} do ${nomes[comissoes.indexOf(menor)]}`)
 }
+function exe4Prova() {
+    let modelos = []
+    let quantidades = []
+    let classificacoes = []
+    //entrada de dados
+    for (let i = 0; i < 10; i++) {
+        let modelo = prompt('Digite o nome do modelo de computador')
+        let quantidade = parseInt(prompt('Digite a quantidade em estoque'))
+        let classificacao = prompt('Digite a classificação (gamer, notebook, desktop)')
+        //verifica se a quantidade em estoque é válida
+        if (quantidade >= 0) {
+            modelos.push(modelo)
+            quantidades.push(quantidade)
+            classificacoes.push(classificacao)
+        } else {
+            alert('Quantidade em estoque não pode ser negativa')
+            i-- 
+        }
+    }
+    let compras = 0
+    let notebookMaisEstoque = ''
+    let notebookMenosEstoque = ''
+    let maiorEstoque = -1
+    let menorEstoque = 9999999
+    //recebe os dados de compra
+    while (compras < 4) {
+        let modeloCompra = prompt(`Digite o modelo do computador que deseja comprar ${modelos}`)
+        let quantidadeCompra = parseInt(prompt('Digite a quantidade que deseja comprar'))
+        //verifica se o modelo existe e a quantidade é suficiente
+        let indiceModelo = modelos.indexOf(modeloCompra)
+        if (indiceModelo !== -1 && quantidadeCompra <= quantidades[indiceModelo]) {
+            quantidades[indiceModelo] -= quantidadeCompra 
+            compras++
+            //verifica se o modelo é do tipo notebook e atualiza o maior e menor estoque
+            if (classificacoes[indiceModelo] === 'notebook') {
+                if (quantidades[indiceModelo] > maiorEstoque) {
+                    maiorEstoque = quantidades[indiceModelo]
+                    notebookMaisEstoque = modeloCompra
+                }
+                if (quantidades[indiceModelo] < menorEstoque) {
+                    menorEstoque = quantidades[indiceModelo]
+                    notebookMenosEstoque = modeloCompra
+                }
+            }
+            alert('Compra realizada')
+        } else {
+            alert('Compra cancelada\nModelo não encontrado ou quantidade insuficiente')
+        }
+    }
+    //modelos de notebooks com maior e menor estoque
+    alert(`Modelo de notebook com maior estoque =  ${notebookMaisEstoque}, ${maiorEstoque}`)
+    alert(`Modelo de notebook com menor estoque = ${notebookMenosEstoque}, ${menorEstoque}`)
+}
+
+/* Analise as afirmações sobre as linguagens aprendidas até aqui no curso
+
+I. HTML é uma linguagem de programação e de marcação de elementos no front-end
+II. Java Script é uma linguagem de programação no front-end e precisa de um browser para interpretá-la
+III. Um projeto que segue as melhores práticas de programação possui 3 arquivos separados por suas funcionalidades: 1 arquivo HTML, 1 arquivo CSS e um arquivo JavaScript, sendo que o HTML possui referência para os arquivos CSS e JS
+
+Estão corretas
+
+I, II e III
+
+Apenas I e II
+
+Apenas I e III
+
+Apenas II e III
+
+Analise as afirmações a respeito da linguagem JavaScript
+
+I. Os operadores lógico &&, || e ! são respectivamente operadores e, ou e negação
+II. Os comandos = e == são, respectivamente, de atribuição e comparação
+III. Não é preciso definir o tipo de dado de uma variável em JavaScript, ela pode assumir diferentes tipos ao longo de sua execução
+
+Estão corretas
+
+I, II e III
+
+Apenas I e II
+
+Apenas I e III
+
+Apenas II e III
+
+Analise as afirmações a respeito das estruturas de repetição:
+  
+I. os comandos while e for possuem teste no início, enquanto que os comandos do ... while possuem teste no final
+  
+II. o comando do ... while é bom para verificar se entrada de dados do usuário está correta, colocando um loop que se encerra apenas quando o usuário informar valor correto.
+  
+III. o comando while precisa conter o comando de incremento(i++)/decremento(i--) para que em algum momento, o laço acabe. O comando for não precisa deste comando de incremento(i++)/decremento(i--).
+
+Estão corretas:
+
+I e II
+
+I, II e III
+
+I e III
+
+II e III */
