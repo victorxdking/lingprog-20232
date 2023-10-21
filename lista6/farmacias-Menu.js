cdbb3c3ac18cb8895c7e97f665afabd866bdb831
victorxdking <victorxdking@gmail.com>
2023-10-20 21:05:33 -0300

    tarefa de modularização e criação de menu

--- lista6/farmacias-Menu.js
+++ lista6/farmacias-Menu.js
@@ -1,90 +1,89 @@
-// cadastra as farmácias
-function cadastraFarmacia(vetor) {
-    let farmacias = []
+function cadastraFarmacia(farmacias) {
     let objeto = {
         codigo: parseInt(prompt(`Informe o código da farmácia`)),
         nome: prompt(`Informe o nome da farmácia`),
         endereco: prompt(`Informe o endereço da farmácia`)
     }
-    while (farmacias.some( (item) => item.codigo == objeto.codigo )){
-        objeto.codigo = parseInt(prompt(`Código já existente. Informe outro código`))
+
+    while (farmacias.some((item) => item.codigo === objeto.codigo)) {
+        objeto.codigo = parseInt(prompt(`Código já existente. Informe outro código`));
     }
-    farmacias.push(objeto)
-    console.log('Farmacia inserida com sucesso')
-    return farmacias
+
+    farmacias.push(objeto);
+    console.log('Farmácia cadastrada com sucesso');
 }
 
-// cadastra os remédios
-function cadastraRemedio(vetor){
-    let remedios = []
+function cadastraRemedio(farmacias, remedios) {
     let objeto = {
         codFarmacia: parseInt(prompt('Código da farmácia')),
         nome: prompt('Nome do remédio').toUpperCase(),
         qtde: parseInt(prompt('Qtde do remédio')),
         preco: parseFloat(prompt('Preço do remédio'))
     }
-    for(let i=0; i > vetor.length; i++){
-    
-        while(!vetor[i].farmacias.some( (item) => item.codigo == objeto.codFarmacia)){
-            objeto.codFarmacia = parseInt(prompt('Farmácia não existe. Informe novamente o código'))
-        }
-        let indice = remedios.findIndex( (item) => item.codFarmacia == objeto.codFarmacia && item.nome == objeto.nome)
-        if (indice == -1){ // remedio não existe
-            remedios.push(objeto) // insere remédio
-        }
-        else { // já existe, atualiza estoque
-            remedios[indice].qtde = remedios[indice].qtde + objeto.qtde
-        }
 
+    while (!farmacias.some((item) => item.codigo === objeto.codFarmacia)) {
+        objeto.codFarmacia = parseInt(prompt('Farmácia não existe. Informe novamente o código'));
+    }
+
+    let indice = remedios.findIndex((item) => item.codFarmacia === objeto.codFarmacia && item.nome === objeto.nome);
+
+    if (indice === -1) {
+        remedios.push(objeto);
+        console.log('Remédio cadastrado com sucesso');
+    } else {
+        remedios[indice].qtde += objeto.qtde;
+        console.log('Estoque do remédio atualizado com sucesso');
     }
 }
 
-// 5 vendas
-function cadastraVenda(vetor){
+function realizaVenda(remedios) {
     let venda = {
         farmacia: parseInt(prompt('Informe código da farmácia')),
         remedio: prompt('Informe nome do remédio').toUpperCase(),
         qtde: parseInt(prompt('Informe qtde para compra'))
     }
-    // verifica se existem - some retorna true ou false
-    for(let i=0; i > vetor.length; i++){
-        if (vetor[i].remedios.some( (item) =>
-            (vetor[i].item.codFarmacia == venda.farmacia && item.remedio == venda.remedio) )){
-                // recupera o índice do remédio - findIndex retorna o índice do vetor
-                let indice = remedios.findIndex((item => 
-                    item.nome == venda.remedio && item.codFarmacia == venda.remedio))
-                if (venda.qtde <= remedios[indice].qtde){
-                    // atualiza o estoque
-                    remedios[indice].qtde = remedios[indice].qtde - venda.qtde
-                    alert('Venda realizada com sucesso')
-                    console.log('Venda realizada com sucesso')
-                }
-                else {
-                    alert('Estoque insuficiente')
-                    console.log('Estoque insuficiente')
-                }
+
+    if (remedios.some((item) => item.codFarmacia === venda.farmacia && item.nome === venda.remedio)) {
+        let indice = remedios.findIndex((item) => item.codFarmacia === venda.farmacia && item.nome === venda.remedio);
+
+        if (venda.qtde <= remedios[indice].qtde) {
+            remedios[indice].qtde -= venda.qtde;
+            console.log('Venda realizada com sucesso');
+        } else {
+            console.log('Estoque insuficiente');
         }
-        else {
-                alert('Farmácia ou remédio inexistente')
-                console.log('Farmácia ou remédio inexistente')
-        }
+    } else {
+        console.log('Farmácia ou remédio inexistente');
     }
 }
 
-// menu
-function principal(){
-    let vetor = []
-    let opcao
+function principal() {
+    let farmacias = [];
+    let remedios = [];
+
+    let opcao;
+
     do {
-        opcao = Number(prompt("Informe a opção escolhida: \n 1.Cadastra farmacia \n 2.Cadastra remédio \n 3.Cadastra venda \n 4.Sair"))
-        switch(opcao){
-            case 1: cadastraFarmacia(vetor); break
-            case 2: cadastraRemedio(vetor); break
-            case 3: cadastraVenda(vetor); break
-            case 4: alert('Programa será encerrado'); break
-            default: alert('Opção inválida');
+        opcao = Number(prompt("Informe a opção escolhida: \n 1.Cadastrar Farmácia \n 2.Cadastrar Remédio \n 3.Realizar Venda \n 4.Sair"));
+
+        switch (opcao) {
+            case 1:
+                cadastraFarmacia(farmacias);
+                break;
+            case 2:
+                cadastraRemedio(farmacias, remedios);
+                break;
+            case 3:
+                realizaVenda(remedios);
+                break;
+            case 4:
+                alert('Programa será encerrado');
+                break;
+            default:
+                alert('Opção inválida');
         }
-    }
-    while (opcao != 4)
+    } while (opcao !== 4);
 }
-principal()
+
+// Chamando a função principal para iniciar o programa
+principal();
